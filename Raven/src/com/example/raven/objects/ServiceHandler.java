2636f82905ab6fa69e639037d672becdaae1385d
Eran <eranno@post.jce.ac.il>
2014-05-03 08:57:22 +0300

    Added functions check for internet connection

--- Raven/src/com/example/raven/objects/ServiceHandler.java
+++ Raven/src/com/example/raven/objects/ServiceHandler.java
@@ -18,6 +18,9 @@ import org.apache.http.util.EntityUtils;
 
 import android.os.AsyncTask;
 //import android.os.StrictMode;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
  
 public class ServiceHandler {
 
@@ -25,10 +28,45 @@ public class ServiceHandler {
     public final static int GET = 1;
     public final static int POST = 2;
 
+    public static int TYPE_WIFI = 1;
+    public static int TYPE_MOBILE = 2;
+    public static int TYPE_NOT_CONNECTED = 0;
+    
     public ServiceHandler() {
  
     }
 
+
+    public static int getConnectivityStatus(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+ 
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        if (null != activeNetwork) {
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_WIFI)
+                return TYPE_WIFI;
+             
+            if(activeNetwork.getType() == ConnectivityManager.TYPE_MOBILE)
+                return TYPE_MOBILE;
+        } 
+        return TYPE_NOT_CONNECTED;
+    }
+
+
+    public static String getConnectivityStatusString(Context context) {
+        int conn = ServiceHandler.getConnectivityStatus(context);
+        String status = null;
+        if (conn == ServiceHandler.TYPE_WIFI) {
+            status = "Wifi enabled";
+        } else if (conn == ServiceHandler.TYPE_MOBILE) {
+            status = "Mobile data enabled";
+        } else if (conn == ServiceHandler.TYPE_NOT_CONNECTED) {
+            status = "Not connected to Internet";
+        }
+        return status;
+    }
+    
+    
     private class ConnectionTask extends AsyncTask<Object, Void, String> {
    	 
         @Override
