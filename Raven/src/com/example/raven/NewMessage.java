1f52d39e74225ce37d040a3aa0758c97428afc7d
Vladimir Novikov <kakifish@hotmail.com>
2014-06-25 23:12:15 +0300

    contacts num only

--- Raven/src/com/example/raven/NewMessage.java
+++ Raven/src/com/example/raven/NewMessage.java
@@ -1,10 +1,14 @@
 package com.example.raven;
 
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
@@ -24,6 +28,19 @@ public class NewMessage extends Activity
 		setContentView(R.layout.activity_new_message);
 		
 		mTxtPhoneNo = (AutoCompleteTextView) findViewById(R.id.mmWhoNo);
+		mTxtPhoneNo.setOnItemClickListener(new OnItemClickListener()
+		{
+			@SuppressWarnings("unchecked")
+			@Override
+			public void onItemClick(AdapterView<?> av, View arg1, int index,
+					long arg3)
+			{
+				Map<String, String> map = (Map<String, String>) av
+						.getItemAtPosition(index);
+				String number = map.get("Phone");
+				mTxtPhoneNo.setText("" + number);
+			}
+		});
 		
 		mAdapter = new SimpleAdapter(this, HistoryActivity.mPeopleList,
 				R.layout.custcontview,
@@ -31,6 +48,13 @@ public class NewMessage extends Activity
 						R.id.ccontName, R.id.ccontNo, R.id.ccontType });
 		
 		mTxtPhoneNo.setAdapter(mAdapter);
+	}
+	
+	public void onContactClick(View v)
+	{
+		String[] contactTokens = mTxtPhoneNo.getText().toString().split("=");
+		String phoneNum = contactTokens[contactTokens.length - 1];
+		mTxtPhoneNo.setText(phoneNum);
 	}
 	
 	@Override
@@ -44,10 +68,10 @@ public class NewMessage extends Activity
 	public void onSendClick(View v)
 	{
 		EditText phoneTxt = (EditText) findViewById(R.id.mmWhoNo);
-		String phoneNum = phoneTxt.getText().toString();
+		String phoneNum = phoneTxt.getText().toString().trim();
 		MultiAutoCompleteTextView smsTxt = (MultiAutoCompleteTextView) findViewById(R.id.SmsTxt);
-		String messageTxt = smsTxt.getText().toString();
-		dal.addMessage(messageTxt, null, "0545424151", 0, 1, 1);
+		String messageTxt = smsTxt.getText().toString().trim();
+		dal.addMessage(messageTxt, null, phoneNum, 0, 1, 1);
 	}
 	
 	public void onContactsClick(View v)
