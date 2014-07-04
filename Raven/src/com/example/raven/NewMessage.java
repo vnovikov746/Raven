4256043c325b3e8cba18116423f143a06028f61d
Eran <eranno@post.jce.ac.il>
2014-07-04 13:25:04 +0300

    update NewMessage to use SmsSender

--- Raven/src/com/example/raven/NewMessage.java
+++ Raven/src/com/example/raven/NewMessage.java
@@ -11,6 +11,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
@@ -29,20 +30,20 @@ import com.example.raven.db.RavenDAL;
 import com.example.raven.dict.Translator;
 import com.example.raven.objects.CountryCodeMap;
 import com.example.raven.objects.Raven;
+import com.example.raven.objects.SmsSender;
 
 public class NewMessage extends Activity
 {
 	private AutoCompleteTextView mTxtPhoneNo;
-	
+	private SmsSender sender;
 	private RavenDAL dal = HistoryActivity.dal;
-	
-	// private NewMessageAdapter mca;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_message);
+		sender = new SmsSender(this);
 		
 		mTxtPhoneNo = (AutoCompleteTextView) findViewById(R.id.mmWhoNo);
 		MultiAutoCompleteTextView smsTxt = (MultiAutoCompleteTextView) findViewById(R.id.SmsTxt);
@@ -119,68 +120,22 @@ public class NewMessage extends Activity
 	public void onSendClick(View v)
 	{
 		String phoneNo = mTxtPhoneNo.getText().toString().trim();
-		
-		// TelephonyManager tm = (TelephonyManager)
-		// getSystemService(Context.TELEPHONY_SERVICE);
-		// String countryCode = tm.getSimCountryIso();
-		// if(phoneNo.startsWith("0"))
-		// {
-		// phoneNo = CountryCodeMap.COUNTRIES.get(countryCode)
-		// + phoneNo.substring(1);
-		// }
-		
 		MultiAutoCompleteTextView smsTxt = (MultiAutoCompleteTextView) findViewById(R.id.SmsTxt);
 		String message = smsTxt.getText().toString().trim();
-		
-
 		
 		
 		if(phoneNo.length() > 0 && message.length() > 0)
 		{
-			
-			//translation
-			Translator t = Raven.SetService(Raven.YANDEX);
-			String translated = t.translate("he", "en", message);
-			String original = message;
-			
-			if (translated == "")
-				message = original;
-			else
-				message = translated;
-			
-			//sms send
-			TelephonyManager telMgr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-			int simState = telMgr.getSimState();
-			switch(simState)
+
+			if(!dal.isContactExist(phoneNo))
 			{
-				case TelephonyManager.SIM_STATE_ABSENT:
-					displayAlert();
-					break;
-				case TelephonyManager.SIM_STATE_NETWORK_LOCKED:
-					// do something
-					break;
-				case TelephonyManager.SIM_STATE_PIN_REQUIRED:
-					// do something
-					break;
-				case TelephonyManager.SIM_STATE_PUK_REQUIRED:
-					// do something
-					break;
-				case TelephonyManager.SIM_STATE_READY:
-					// do something
-					if(!dal.isContactExist(phoneNo))
-					{
-						dal.addContact("", phoneNo, "temp", "", 0);
-					}
-					sendSMS(phoneNo, message); // method to send message
-					
-					Intent intent = new Intent(this, Chat.class);
-					intent.putExtra("phoneNum", phoneNo);
-					startActivity(intent);
-					break;
-				case TelephonyManager.SIM_STATE_UNKNOWN:
-					// do something
-					break;
+				dal.addContact("", phoneNo, "temp", "", 0);
 			}
+			sender.send(phoneNo, message);
+			
+			Intent intent = new Intent(this, Chat.class);
+			intent.putExtra("phoneNum", phoneNo);
+			startActivity(intent);
 		}
 		else
 		{
@@ -189,105 +144,7 @@ public class NewMessage extends Activity
 					Toast.LENGTH_SHORT).show();
 		}
 	}
-	
-	private void displayAlert()
-	{
-		new AlertDialog.Builder(NewMessage.this)
-				.setMessage("Sim card not available").setCancelable(false)
-				.setPositiveButton("ok", new DialogInterface.OnClickListener()
-				{
-					@Override
-					public void onClick(DialogInterface dialog, int id)
-					{
-						Log.d("I am inside ok", "ok");
-						dialog.cancel();
-					}
-				}).show();
-	}
-	
-	private void sendSMS(String phoneNumber, String message)
-	{
-		String SENT = "SMS_SENT";
-		String DELIVERED = "SMS_DELIVERED";
-		
-		TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-		
-		String countryCode = CountryCodeMap.COUNTRIES
-				.get(tm.getSimCountryIso());
-		if(phoneNumber.startsWith(countryCode))
-		{
-			phoneNumber = "0" + phoneNumber.substring(countryCode.length());
-		}
-		
-		dal.addMessage(message, null, phoneNumber, Constants.SENT_BY_ME,
-				Constants.NOT_READ, Constants.NOT_SENT);
-		
-		PendingIntent sentPI = PendingIntent.getBroadcast(NewMessage.this, 0,
-				new Intent(SENT), 0);
-		
-		PendingIntent deliveredPI = PendingIntent.getBroadcast(NewMessage.this,
-				0, new Intent(DELIVERED), 0);
-		
-		// ---when the SMS has been sent---
-		registerReceiver(new BroadcastReceiver()
-		{
-			
-			@Override
-			public void onReceive(Context arg0, Intent arg1)
-			{
-				switch(getResultCode())
-				{
-					case Activity.RESULT_OK:
-						// Toast.makeText(NewMessage.this, "SMS sent",
-						// Toast.LENGTH_SHORT).show();
-						Log.d("NEW MESSAGE SMS", "SMS SENT");
-						break;
-					case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-						Toast.makeText(NewMessage.this, "Generic failure",
-								Toast.LENGTH_SHORT).show();
-						break;
-					case SmsManager.RESULT_ERROR_NO_SERVICE:
-						Toast.makeText(NewMessage.this, "No service",
-								Toast.LENGTH_SHORT).show();
-						break;
-					case SmsManager.RESULT_ERROR_NULL_PDU:
-						Toast.makeText(NewMessage.this, "Null PDU",
-								Toast.LENGTH_SHORT).show();
-						break;
-					case SmsManager.RESULT_ERROR_RADIO_OFF:
-						Toast.makeText(getBaseContext(), "Radio off",
-								Toast.LENGTH_SHORT).show();
-						break;
-				
-				}
-			}
-		}, new IntentFilter(SENT));
-		
-		// ---when the SMS has been delivered---
-		registerReceiver(new BroadcastReceiver()
-		{
-			@Override
-			public void onReceive(Context arg0, Intent arg1)
-			{
-				switch(getResultCode())
-				{
-					case Activity.RESULT_OK:
-						// Toast.makeText(Chat.this, "SMS delivered",
-						// Toast.LENGTH_LONG).show();
-						Log.d("SMS NEW MESSAGE", "DELIVERED");
-						break;
-					case Activity.RESULT_CANCELED:
-						Toast.makeText(NewMessage.this, "SMS not delivered",
-								Toast.LENGTH_LONG).show();
-						break;
-				}
-			}
-		}, new IntentFilter(DELIVERED));
-		
-		SmsManager sms = SmsManager.getDefault();
-		sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
-	}
-	
+
 	public void onContactsClick(View v)
 	{
 		Intent intent = new Intent(this, Contacts.class);
